ee11a9a8eb5eae2912f4d07d4123e4565d1b5e68
Nikita-Dudorov <zoom_ccss@mail.ru>
2023-11-21 22:55:07 +0300

    best config

--- config.py
+++ config.py
@@ -8,8 +8,8 @@ class Args:
         self.n_env_steps = 1e5
         self.rollout_len = 256
         self.batch_size = 64
-        self.n_epochs = 1  # 20
-        self.lr = 1e-5  # 1e-3
+        self.n_epochs = 10 
+        self.lr = 1e-3
         self.lr_decay = True
         self.grad_clip = 0.5
 
@@ -25,6 +25,6 @@ class Args:
         self.ppo_eps = 0.2
         self.ppo_eps_decay = True
         self.gamma = 0.98
-        self.gae_lam = 0.8
+        # self.gae_lam = 0.8
         self.c_val_loss = 0.5
         self.c_entr_loss = 0.0
