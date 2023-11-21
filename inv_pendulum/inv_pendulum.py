c2b51919fa3571ed4b6faba400bbf7b184cc9304
Nikita-Dudorov <zoom_ccss@mail.ru>
2023-11-22 02:51:20 +0300

    setup inv pendul

--- inv_pendulum/inv_pendulum.py
+++ inv_pendulum/inv_pendulum.py
@@ -5,6 +5,7 @@ from gym.envs.mujoco import MujocoEnv
 from gym.spaces import Box
 
 class InvertedPendulumEnv(MujocoEnv, utils.EzPickle):
+    MAX_EP_LEN = 1000
     metadata = {
         "render_modes": [
             "human",
@@ -21,25 +22,29 @@ class InvertedPendulumEnv(MujocoEnv, utils.EzPickle):
         observation_space = Box(low=-np.inf, high=np.inf, shape=(4,), dtype=np.float64)
         MujocoEnv.__init__(
             self,
-            "/home/nikita/Projects/RL/PPO/env/model.xml",
+            "/home/nikita/Projects/RL/PPO/inv_pendulum/model.xml",
             2,
             observation_space=observation_space,
             **kwargs
         )
         self.last_ob = None
+        self._timestep = 0 
 
     def step(self, a):
-        reward = 1.0
+        self._timestep += 1
+        reward = 1.0  # change it to angle diff with vertical pos
         self.do_simulation(a, self.frame_skip)
 
         ob = self._get_obs()
         terminated = bool(not np.isfinite(ob).all())
+        truncated = self._timestep >= self.MAX_EP_LEN
 
         if self.render_mode == "human":
             self.render()
-        return ob, reward, terminated, False, {}
+        return ob, reward, terminated, truncated, {}
 
     def reset_model(self):
+        self._timestep = 0
         qpos = self.init_qpos
         qvel = self.init_qvel
         qpos[1] = 3.14 # Set the pole to be facing down
